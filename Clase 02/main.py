ec8b03193d21476ce02a1828deca00d2decfd0bd
tomas <tomizampellarp@gmai.com>
2023-09-17 18:12:46 -0300

    agrego tp 02 stark con los puntos de g-h-i-j a revisar

--- Clase 02/main.py
+++ Clase 02/main.py
@@ -1,32 +1,6 @@
 from data import lista_personajes
+#TP Stark 01 - Tomas Lautaro Diaz Zampella
 
-"""
-Luego de analizar el set de datos correspondiente resolver el Desafío #01:
-
-A. Recorrer la lista imprimiendo por consola todos los datos de cada superhéroe
-B. Recorrer la lista y mostrar la identidad y el peso del superhéroe con mayor
-fuerza (MÁXIMO)
-C. Recorrer la lista y mostrar nombre e identidad del superhéroe más bajo
-(MÍNIMO)
-D. Recorrer la lista y determinar el peso promedio de los superhéroes
-masculinos (PROMEDIO)
-E. Recorrer la lista y mostrar nombre y peso de los superhéroes (cualquier
-género) los cuales su fuerza supere a la fuerza promedio de todas las
-superhéroes de género femenino
-
-NOTA: Se debe construir un menú en el que se sea posible acceder a cada una de
-las opciones (A-E)
-"nombre": "Rocket Raccoon",
-    "identidad": "Rocket Raccoon",
-    "empresa": "Marvel Comics",
-    "altura": "122.77",
-    "peso": "25.73",
-    "genero": "M",
-    "color_ojos": "Brown",
-    "color_pelo": "Brown",
-    "fuerza": "5",
-    "inteligencia": "average"
-"""
 maximo_fuerza = None
 minimo_altura = None
 contador_masculino = 0
@@ -34,79 +8,84 @@ acumulador_peso_masc = 0
 contador_femenino = 0
 acumulador_fuerza_fem = 0
 
+while (True):
+    print("Menú:")
+    print("A. Imprime todos los datos de cada superhéroe")
+    print("B. Imprime la identidad y el peso del superhéroe con mayor fuerza")
+    print("C. Imprime el nombre e identidad del superhéroe más bajo")
+    print("D. Imprime el peso promedio de los superhéroes masculinos")
+    print("E. Imprime nombre y peso de los superhéroes los cuales su fuerza supere a la fuerza promedio de todas las superhéroes de género femenino")
+    print("F. Salir\n")
 
-print("A. Imprime todos los datos de cada superhéroe")
-print("B. Imprime la identidad y el peso del superhéroe con mayor fuerza")
-print("C. Imprime el nombre e identidad del superhéroe más bajo")
-print("D. Imprime el peso promedio de los superhéroesmasculinos")
-print("E. Imprime nombre y peso de los superhéroes los cuales su fuerza supere a la fuerza promedio de todas las superhéroes de género femenino\n")
-
-opcion_elegida = input("Seleccione una letra: ")
+    opcion_elegida = input("Seleccione una letra: ")
 
 
-match opcion_elegida:
-    case "A" | "a":
-        for heroe in lista_personajes:
-            print("Nombre:", heroe["nombre"])
-            print("Identidad:", heroe["identidad"])
-            print("Altura:", heroe["altura"])
-            print("Peso:", heroe["peso"])
-            print("Genero:", heroe["genero"])
-            print("Color de ojos:", heroe["color_ojos"])
-            print("Color de pelo:", heroe["color_pelo"])
-            print("Fuerza:", heroe["fuerza"])
-            print("inteligencia:", heroe["inteligencia"])
-            print("")
-            
-
-    case "B" | "b":
-        for heroe in lista_personajes:
-            #B:superhéroe con mayor fuerza
-            if maximo_fuerza == None or maximo_fuerza < int(heroe["fuerza"]):
-                maximo_fuerza = int(heroe["fuerza"])
-        
-        print("Superhéroes con mayor fuerza")
-        for heroe in lista_personajes:
-            if maximo_fuerza == int(heroe["fuerza"]): #B
-                print("Identidad:", heroe["identidad"])
-                print("Peso:", float(heroe["peso"]), "kg")
-                print("")
-    
-    case "C" | "c":
-        for heroe in lista_personajes:
-            #C:nombre e identidad del superhéroe más bajo
-            if minimo_altura == None or minimo_altura > float(heroe["altura"]):
-                minimo_altura = float(heroe["altura"])
-                minimo_altura_identidad = heroe["identidad"]
-                minimo_altura_nombre = heroe["nombre"]
-        print (f"El superhéroe más bajo es {minimo_altura_nombre} y su identidad es {minimo_altura_identidad}") #C
-    
-    case "D" | "d":
-        for heroe in lista_personajes:
-            #D: D. Recorrer la lista y determinar el peso promedio de los superhéroes masculinos (PROMEDIO)
-            if heroe["genero"] == "M":
-                    contador_masculino = contador_masculino + 1
-                    acumulador_peso_masc = acumulador_peso_masc + float(heroe["peso"])
-        
-        promedio_peso_masc = acumulador_peso_masc / contador_masculino        
-        print (f"El peso promedio de los superhéroes masculinos es {promedio_peso_masc :.0f} kg") #D       
-
-    case "E" | "e":
-        for heroe in lista_personajes:
-            #E: Recorrer la lista y mostrar nombre y peso de los superhéroes (cualquier género) los cuales su fuerza supere a la fuerza promedio de todas lassuperhéroes de género femenino
-            if heroe["genero"] == "F":
-                contador_femenino = contador_femenino + 1 #E
-                acumulador_fuerza_fem = acumulador_fuerza_fem + float(heroe["fuerza"])     
-        
-        promedio_fuerza_fem = acumulador_fuerza_fem / contador_femenino
-        
-        print("Nombre y peso de los superhéroes los cuales su fuerza supere a la fuerza promedio de todas las superhéroes de género femenino\n")
-        for heroe in lista_personajes:
-            if float(heroe["fuerza"]) > promedio_fuerza_fem:
+    match opcion_elegida:
+        case "A" | "a":
+            for heroe in lista_personajes:
                 print("Nombre:", heroe["nombre"])
-                print("Peso:", float(heroe["peso"]),"kg")
-                print ("---------------------")     
+                print("Identidad:", heroe["identidad"])
+                print("Altura:", heroe["altura"])
+                print("Peso:", heroe["peso"])
+                print("Genero:", heroe["genero"])
+                print("Color de ojos:", heroe["color_ojos"])
+                print("Color de pelo:", heroe["color_pelo"])
+                print("Fuerza:", heroe["fuerza"])
+                print("inteligencia:", heroe["inteligencia"], "\n")
+                
+                
 
-    case _:
-        print("Ingrese una letra de la A a la E")         
-             
+        case "B" | "b":
+            for heroe in lista_personajes:
+        
+                if maximo_fuerza == None or maximo_fuerza < int(heroe["fuerza"]):
+                    maximo_fuerza = int(heroe["fuerza"])
+            
+            print("Superhéroes con mayor fuerza")
+            for heroe in lista_personajes:
+                if maximo_fuerza == int(heroe["fuerza"]): #B
+                    print("Identidad:", heroe["identidad"])
+                    print("Peso:", float(heroe["peso"]), "kg")
+                    print("")
+        
+        case "C" | "c":
+            for heroe in lista_personajes:
+                
+                if minimo_altura == None or minimo_altura > float(heroe["altura"]):
+                    minimo_altura = float(heroe["altura"])
+                    minimo_altura_identidad = heroe["identidad"]
+                    minimo_altura_nombre = heroe["nombre"]
+            print (f"El superhéroe más bajo es {minimo_altura_nombre} y su identidad es {minimo_altura_identidad} \n") #C
+        
+        case "D" | "d":
+            for heroe in lista_personajes:
+                
+                if heroe["genero"] == "M":
+                        contador_masculino = contador_masculino + 1
+                        acumulador_peso_masc = acumulador_peso_masc + float(heroe["peso"])
+            
+            promedio_peso_masc = acumulador_peso_masc / contador_masculino        
+            print (f"El peso promedio de los superhéroes masculinos es {promedio_peso_masc :.0f} kg\n") #D       
+
+        case "E" | "e":
+            for heroe in lista_personajes:
+                
+                if heroe["genero"] == "F":
+                    contador_femenino = contador_femenino + 1 #E
+                    acumulador_fuerza_fem = acumulador_fuerza_fem + float(heroe["fuerza"])     
+            
+            promedio_fuerza_fem = acumulador_fuerza_fem / contador_femenino
+            
+            print("Nombre y peso de los superhéroes los cuales su fuerza supere a la fuerza promedio de todas las superhéroes de género femenino\n")
+            for heroe in lista_personajes:
+                if float(heroe["fuerza"]) > promedio_fuerza_fem:
+                    print("Nombre:", heroe["nombre"])
+                    print("Peso:", float(heroe["peso"]),"kg\n")
+                       
+
+        case "F" | "f":
+            break
+
+        case _:
+            print("Ingrese una letra de la A a la F")         
+                
---
+++ Clase 03/main.py
@@ -0,0 +1,283 @@
+from data import lista_personajes
+#TP Stark 02 - Tomas Lautaro Diaz Zampella
+
+#A
+def muestra_nombres_heroe_genero_nb ():
+    '''Muestra los nombres de los superhéroes de género NB'''
+    for heroe in lista_personajes:
+            if heroe["genero"] == "NB":
+                print("Nombre:", heroe["nombre"])
+
+#B
+def calcular_heroe_mas_alto_femenino ():
+    '''Calcula el superhéroe más alto de género F'''
+    maxima_altura_f = None
+    maxima_altura_f_nombre = None
+    for heroe in lista_personajes:
+            if heroe["genero"] == "F":
+                 if maxima_altura_f == None or maxima_altura_f < float(heroe["altura"]):
+                    maxima_altura_f = float(heroe["altura"])
+                    maxima_altura_f_nombre = heroe["nombre"]
+    print (f"El superhéroe más alto de genero femenino es {maxima_altura_f_nombre} \n")
+
+#C
+def calcular_heroe_mas_alto_masculino ():
+    '''Calcula el superhéroe más alto de género M'''
+    maxima_altura_m = None
+    maxima_altura_m_nombre = None
+    for heroe in lista_personajes:
+            if heroe["genero"] == "M":
+                 if maxima_altura_m == None or maxima_altura_m < float(heroe["altura"]):
+                    maxima_altura_m = float(heroe["altura"])
+                    maxima_altura_m_nombre = heroe["nombre"]
+    print (f"El superhéroe más alto de genero masculino es {maxima_altura_m_nombre} \n")
+
+#D
+def calcular_heroe_mas_debil_masculino ():
+    '''Calcula el superhéroe más débil de género M'''
+    minima_fuerza_m = None
+    minima_fuerza_m_nombre = None
+    for heroe in lista_personajes:
+            if heroe["genero"] == "M":
+                 if minima_fuerza_m == None or minima_fuerza_m > float(heroe["fuerza"]):
+                    minima_fuerza_m = float(heroe["fuerza"])
+                    minima_fuerza_m_nombre = heroe["nombre"]
+    print (f"El superhéroe más débil de genero masculino es {minima_fuerza_m_nombre} \n")
+
+#E
+def calcular_heroe_mas_debil_no_binario ():
+    '''Calcula el superhéroe más débil de género NB'''
+    minima_fuerza_nb = None
+    minima_fuerza_nb_nombre = None
+    for heroe in lista_personajes:
+            if heroe["genero"] == "NB":
+                 if minima_fuerza_nb == None or minima_fuerza_nb > float(heroe["fuerza"]):
+                    minima_fuerza_nb = float(heroe["fuerza"])
+                    minima_fuerza_nb_nombre = heroe["nombre"]
+    if minima_fuerza_nb_nombre != None:
+        print (f"El superhéroe más débil de genero no binario es {minima_fuerza_nb_nombre} \n")
+
+#F
+def calcular_fuerza_promedio_genero_nb ():
+    '''Calcula la fuerza promedio de los superhéroes de género NB'''
+    contador_no_binario = 0
+    acumulador_fuerza_nb = 0
+    for heroe in lista_personajes:
+                
+                if heroe["genero"] == "NB":
+                        contador_no_binario = contador_no_binario + 1
+                        acumulador_fuerza_nb = acumulador_fuerza_nb + float(heroe["fuerza"])
+       
+    if contador_no_binario != 0: 
+       promedio_fuerza_nb = acumulador_fuerza_nb / contador_no_binario  
+       print (f"La fuerza promedio de los superhéroes de genero no binario es {promedio_fuerza_nb :.0f} kg\n") #D   
+
+#G
+def calcular_heroes_por_color_ojos ():
+    '''Calcula la cantidad de superhéroes por color de ojos'''
+    
+    contador_red = 0
+    contador_brown = 0
+    contador_yellow_no_irises = 0
+    contador_silver = 0
+    contador_blue = 0
+    contador_yellow = 0
+    contador_hazel = 0
+    contador_green = 0 
+    
+    
+    for heroe in lista_personajes:
+         match heroe["color_ojos"]:
+            case "Red":
+                   contador_red = contador_red + 1
+            case "Brown":
+                   contador_brown = contador_brown + 1
+            case "Yellow (without irises)":
+                   contador_yellow_no_irises = contador_yellow_no_irises + 1
+            case "Silver":
+                   contador_silver = contador_silver + 1
+            case "blue" | "Blue":
+                   contador_blue = contador_blue + 1
+            case "Yellow":
+                     contador_yellow = contador_yellow + 1
+            case "Hazel":
+                     contador_hazel = contador_hazel + 1
+            case "Green":
+                     contador_green = contador_green + 1
+    
+    print(f"Red: {contador_red}\n\
+Brown: {contador_brown}\n\
+Yellow (without irises): {contador_yellow_no_irises}\n\
+Silver: {contador_silver}\n\
+Blue: {contador_blue}\n\
+Yellow: {contador_yellow}\n\
+Hazel: {contador_hazel}\n\
+Green: {contador_green}\n")
+    
+#H
+def calcular_heroes_por_color_pelo ():
+    '''Calcula la cantidad de superhéroes por color de pelo'''
+    
+    contador_black = 0
+    contador_no_hair = 0
+    contador_red_orange = 0
+    contador_blond = 0
+    contador_green = 0
+    contador_brown_white = 0
+    contador_white = 0
+    contador_auburn = 0
+    contador_yellow = 0
+    contador_red = 0
+    contador_brown = 0
+    
+    
+    for heroe in lista_personajes:
+       match heroe["color_pelo"]:
+              case "Black":
+                     contador_black = contador_black + 1
+              case "No Hair" | "":
+                     contador_no_hair = contador_no_hair + 1
+              case "Red / Orange":
+                     contador_red_orange = contador_red_orange + 1
+              case "Blond" | "blond":
+                     contador_blond = contador_blond + 1
+              case "Green":
+                     contador_green = contador_green + 1
+              case "Brown / White":
+                     contador_brown_white = contador_brown_white + 1
+              case "White":
+                     contador_white = contador_white + 1
+              case "Auburn":
+                     contador_auburn = contador_auburn + 1
+              case "Yellow":
+                     contador_yellow = contador_yellow + 1
+              case "Red":
+                     contador_red = contador_red + 1
+              case "Brown":
+                     contador_brown = contador_brown + 1
+            
+    print(f"No hair: {contador_no_hair}\n\
+Black: {contador_black}\n\
+Red / Orange: {contador_red_orange}\n\
+Blond: {contador_blond}\n\
+Green: {contador_green}\n\
+Brown / White: {contador_brown_white}\n\
+White: {contador_white}\n\
+Auburn: {contador_auburn}\n\
+Yellow: {contador_yellow}\n\
+Red: {contador_red}\n\
+Brown: {contador_brown}\n") 
+
+#I
+def listar_heroes_por_color_ojos ():
+    '''Agrupa en lista a superhéroes por color de ojos'''
+    
+    lista_red = []
+    lista_brown = []
+    lista_yellow_no_irises = []
+    lista_silver = []
+    lista_blue = []
+    lista_yellow = []
+    lista_hazel = []
+    lista_green = []
+    
+    
+    for heroe in lista_personajes:
+         match heroe["color_ojos"]:
+            case "Red":
+                   lista_red.append(heroe["nombre"])
+            case "Brown":
+                   lista_brown.append(heroe["nombre"])
+            case "Yellow (without irises)":
+                   lista_yellow_no_irises.append(heroe["nombre"])
+            case "Silver":
+                   lista_silver.append(heroe["nombre"])
+            case "blue" | "Blue":
+                   lista_blue.append(heroe["nombre"])
+            case "Yellow":
+                     lista_yellow.append(heroe["nombre"])
+            case "Hazel":
+                     lista_hazel.append(heroe["nombre"])
+            case "Green":
+                     lista_green.append(heroe["nombre"])
+    
+    print(f"Red: {lista_red}\n\
+Brown: {lista_brown}\n\
+Yellow (without irises): {lista_yellow_no_irises}\n\
+Silver: {lista_silver}\n\
+Blue: {lista_blue}\n\
+Yellow: {lista_yellow}\n\
+Hazel: {lista_hazel}\n\
+Green: {lista_green}\n")
+
+#J
+def listar_heroes_por_tipo_inteligencia ():
+    '''Agrupa en lista a superhéroes por tipo de inteligencia'''
+    lista_sin_inteligencia = []
+    lista_good = []
+    lista_average = []
+    lista_high = []
+    
+    for heroe in lista_personajes:
+         match heroe["inteligencia"]:
+            case "":
+                   lista_sin_inteligencia.append(heroe["nombre"])
+            case "good":
+                   lista_good.append(heroe["nombre"])
+            case "average":
+                   lista_average.append(heroe["nombre"])
+            case "high":
+                   lista_high.append(heroe["nombre"])
+    
+    print(f"Sin inteligencia: {lista_sin_inteligencia}\n\
+good: {lista_good}\n\
+average: {lista_average}\n\
+high: {lista_high}\n")
+
+
+
+
+
+
+
+while (True):
+    print("Menú:")
+    print("A. Nombre de cada superhéroe de género NB")
+    print("B. Superhéroe más alto de género F")
+    print("C. Superhéroe más alto de género M")
+    print("D. Superhéroe más débil de género M")
+    print("E. Superhéroe más débil de género NB")
+    print("F. Fuerza promedio de los superhéroes de género NB")
+    print("G. Cantidad de superhéroes por cada tipo de color de ojos")
+    print("H. Cantidad de superhéroes por cada tipo de color de pelo")
+    print("I. Superhéroes agrupados por color de ojos")
+    print("J. Superhéroes agrupados por tipo de inteligencia")
+    print("K. Salir\n")
+    
+
+    opcion_elegida = input("Seleccione una letra: ")
+
+
+    match opcion_elegida:
+       case "A" | "a":
+              muestra_nombres_heroe_genero_nb ()
+       case "B" | "b":
+              calcular_heroe_mas_alto_femenino()
+       case "C" | "c":
+              calcular_heroe_mas_alto_masculino()
+       case "D" | "d":
+              calcular_heroe_mas_debil_masculino()
+       case "E" | "e":
+              calcular_heroe_mas_debil_no_binario()
+       case "F" | "f":
+              calcular_fuerza_promedio_genero_nb()
+       case "G" | "g":
+              calcular_heroes_por_color_ojos()
+       case "H" | "h":
+              calcular_heroes_por_color_pelo()
+       case "I" | "i":
+              listar_heroes_por_color_ojos()
+       case "J" | "j":
+              listar_heroes_por_tipo_inteligencia()
+       case "K" | "k":
+              break
